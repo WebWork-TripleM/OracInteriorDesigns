3602d12317307741ffc52eef19b0330c6d1c4221
WebWork-TripleM <Mnelisi96010@outlook.com>
2025-05-19 12:45:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Hamburger menu toggle
+document.getElementById('menu-toggle').addEventListener('click', () => {
+    const navList = document.getElementById('nav-list');
+    navList.classList.toggle('show');
+  });
+  
